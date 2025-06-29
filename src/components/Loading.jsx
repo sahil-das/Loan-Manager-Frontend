e13c3cfb7af4adb625e23ab043d340f683a946ce
sahil <sahildas452@gmail.com>
2025-06-29 11:26:09 +0530

    animation

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,12 +1,19 @@
-// Loader.jsx
+// src/components/Loading.jsx
 import React from "react";
+import { Player } from "@lottiefiles/react-lottie-player";
+import loadingAnimation from "../assets/loading.json"; // adjust path
 
-const Loader = () => {
+const Loading = () => {
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-      <div className="w-12 h-12 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
+    <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
+      <Player
+        autoplay
+        loop
+        src={loadingAnimation}
+        style={{ height: "150px", width: "150px" }}
+      />
     </div>
   );
 };
 
-export default Loader;
+export default Loading;
