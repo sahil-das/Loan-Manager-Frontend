33b4f7f84ec30b48bc26cc602ee80741e3998cbe
sahil-das <66070916+sahil-das@users.noreply.github.com>
2025-06-30 21:46:54 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -74,8 +74,7 @@ export default function Navbar() {
         <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
           {/* Logo */}
           <img
-          src="/images/logo.svg
-            "
+          src="logo.svg"
           alt="Borrow Book Logo"
           className="h-10 sm:h-12 w-auto"
           />
